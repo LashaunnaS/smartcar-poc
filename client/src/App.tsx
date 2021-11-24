9247e35164848467f4fde84c76662ffbc356fac0
Lashaunna Samuels <lashaunnasamuels@Lashaunnas-MacBook-Pro-2.local>
2021-11-24 09:03:59 -0500

    optimise loading state to prevent delay in FCP.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,11 @@ import { useEffect, useState } from 'react';
 import Smartcar from '@smartcar/auth';
 import axios from 'axios';
 import './shared/styles.css';
-import { scopeMap } from './shared/scopeMap';
 import Connect from './components/Connect';
 import Vehicle from './components/Vehicle';
 import ErrorFallBack from "./components/ErrorFallBack";
 
-interface ScopeData {
+export interface ScopeData {
   state?: string;
   isPluggedIn?: boolean;
   lifeRemaining?: number;
@@ -30,7 +29,7 @@ export interface ScopeAttributesProps {
   read_engine_oil?: string;
   read_battery?: string,
   read_odometer?: string;
-  read_vehicle_info?: string;
+  read_vehicle_info: string;
   read_fuel?: string;
   read_tires?: string;
   [index: number]: ScopeData;
@@ -38,8 +37,7 @@ export interface ScopeAttributesProps {
 
 const App = (): JSX.Element => {
   const [authCode, setAuthCode] = useState('');
-  const [validScope, setValidScope] = useState([]);
-  const [validScopeAttributes, setValidScopeAttributes] = useState<ScopeAttributesProps[]>([]);
+  const [validScope, setValidScope] = useState<string[]>([]);
   const [thrownError, setThrownError] = useState({
     hasError: false,
     errorMessage: ''
@@ -67,58 +65,35 @@ const App = (): JSX.Element => {
         });
       } else {
         setAuthCode(code);
-        localStorage.setItem('access_token', code);
+        localStorage.setItem('accessToken', code);
 
         // handle the returned code by sending it to your back-end server
         const res = await axios.get(`${process.env.REACT_APP_SERVER}/exchange?code=${code}`);
         setValidScope(res.data.permissions);
+        localStorage.setItem('validScope', res.data.permissions);
       }
     }
   });
 
   useEffect(() => {
-    const accessToken = localStorage.getItem('access_token');
+    const accessToken = localStorage.getItem('accessToken');
     if (accessToken) {
       setAuthCode(accessToken);
     }
-  }, [authCode]);
+  }, []);
 
   useEffect(() => {
-    const scopeData = localStorage.getItem('validScopeAttributes');
-    if (scopeData && validScopeAttributes.length === 0) {
-      setValidScopeAttributes(JSON.parse(scopeData));
+    const scopeKeys = localStorage.getItem('validScope');
+    if (scopeKeys) {
+      setValidScope(scopeKeys.split(','));
     }
-  }, [validScopeAttributes]);
-
-  useEffect(() => {
-    if (authCode !== '' && validScope.length > 0 && validScopeAttributes.length === 0) {
-      const fetchScopeAttributes = async () => {
-        const promises = validScope.map(async (scope: string) => {
-          const response = await axios.get(`${process.env.REACT_APP_SERVER}/${scopeMap[scope].url}`);
-
-          const { meta, id, ...rest } = response.data;
-
-          return [
-            scope,
-            rest
-          ];
-        });
-
-        return await Promise.all(promises).then((data) => setValidScopeAttributes(data));
-      };
-
-      fetchScopeAttributes();
-    };
-
-    validScopeAttributes.length > 0 && localStorage.setItem('validScopeAttributes', JSON.stringify(validScopeAttributes));
-  }, [authCode, validScope, validScopeAttributes]);
+  }, []);
 
   const authorize = () => smartcar.openDialog({ forcePrompt: true });
   const newVehicleAccess = () => {
     window.localStorage.clear();
     setAuthCode('');
     setValidScope([]);
-    setValidScopeAttributes([]);
   };
   const resetErrorBoundry = () => setThrownError({
     hasError: false,
@@ -133,9 +108,9 @@ const App = (): JSX.Element => {
         <h1>SMARTCAR</h1>
         {authCode && <button onClick={newVehicleAccess}>ACCESS NEW VEHICLE</button>}
       </nav>
-      {!authCode ? handleLogin : validScopeAttributes.map((scopeAttributes, index: number) =>
-        <Vehicle scopeAttributes={scopeAttributes} key={`${index}-${scopeAttributes[0]}`} />
-      )}
+      {!authCode ? handleLogin :
+        validScope.map((scope: string, index: number) => <Vehicle scope={scope} key={`${index}-${scope}`} />)
+      }
     </main>
   );
 };
--- client/src/components/Vehicle.tsx
+++ client/src/components/Vehicle.tsx
@@ -1,26 +1,53 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { scopeMap } from "../shared/scopeMap";
 
-const Vehicle = ({ scopeAttributes }: any): JSX.Element => { //TODO: fix type
-  const renderAttributes = (attribute: Record<string, string>) => {
-    let attributeList = [];
+const Vehicle = ({ scope }: any): JSX.Element => {
+  const [validScopeAttributes, setValidScopeAttributes] = useState<any[]>([]);
+
+  useEffect(() => {
+    const scopeData = localStorage.getItem(scope);
+    if (scopeData) {
+      setValidScopeAttributes(JSON.parse(scopeData));
+    };
+  }, []);
+
+  useEffect(() => {
+    const fetchScopeAttributes = async () => {
+      const response = await axios.get(`${process.env.REACT_APP_SERVER}/${scopeMap[scope].url}`);
+      const { meta, id, ...attributes } = response.data;
+
+      setValidScopeAttributes([
+        scope,
+        attributes
+      ]);
+    };
+
+    validScopeAttributes.length === 0 && fetchScopeAttributes();
+
+    validScopeAttributes.length > 0 && localStorage.setItem(scope, JSON.stringify(validScopeAttributes));
+  }, []);
+
+  const renderAttributes = (attribute: Record<string, string | number | boolean>) => {
+    let attributeList: JSX.Element[] = [];
 
     for (const [key, value] of Object.entries(attribute)) {
       attributeList.push(
         <span>{scopeMap[key].name}: {value.toString()}</span>
       );
     }
+
     return attributeList;
   };
 
+  const scopeName = validScopeAttributes[0];
+  const scopeAttributes = validScopeAttributes[1];
+
   return (
     <section className="scope-attribute">
-      <h2>
-        {scopeMap[scopeAttributes[0]].name}
-      </h2>
-
-      <div>
-        {renderAttributes(scopeAttributes[1])}
-      </div>
+      {scopeName && <h2>{scopeMap[scopeName].name}</h2>}
+      {scopeName && <div>{renderAttributes(scopeAttributes)}</div>}
     </section>
   );
 };
